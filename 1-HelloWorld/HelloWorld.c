be801be10a5fa83a9ef15247073811da0b83803f
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2024-05-03 13:59:15 -0300

    Subo carpeta y archivo: 1-HelloWorld

---
+++ 1-HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+// Defines constants, macros, and types, and declares stream input and output functions.
+#include <stdio.h>
+
+int main() {
+    // Print "Hello World!" in console.
+    printf("Hello World!");
+
+    // Return 0 if the code was run successfully.
+    return 0;
+}
